157c8bbd906845aab5f36ad684c3c7db9acfb6c9
oscartison <oscartison@me.com>
2023-12-15 09:24:18 +0000

    accept frontend in cors

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import { app } from 'mu';
 import {queryAssociations, queryAssociationsLocations, queryAssociationsMembers } from './query';
 import createSheet from './sheet';
 
+app.use(cors({
+    origin: 'http://localhost:4200', 
+    methods: 'GET', 
+  }));
+
 app.get('/download', async function(_req, res ) {
     const [associations,locations,members] = await Promise.all([queryAssociations(), queryAssociationsLocations(), queryAssociationsMembers()]);
     const file = await createSheet(associations,locations,members);
